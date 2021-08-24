8d656b294859091b2173651a64cdd2d8700494e9
Youssef <abdelmonemyousef@gmail.com>
2021-08-25 01:37:55 +0200

    changes

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, nulla. Beatae, modi! Possimus incidunt delectus qui maiores magni enim deleniti quam autem optio blanditiis pariatur architecto quos, voluptates officiis adipisci. Voluptatum magni aliquam minus consectetur soluta amet at architecto. Omnis, porro.
+      </p>
+    </section>
+  )
+}
+
+export default About
